15ad23a63fc912e9097349a6a75208e4b1d9f449
vivek <vivek@git.com>
2021-12-01 20:19:56 +0530

    Task #2376 (In Progress): Day 1-2 Plan Angular

--- Typescript/VIVEK DHAGE/day2/assignments/assignment1.ts
+++ Typescript/VIVEK DHAGE/day2/assignments/assignment1.ts
@@ -2,35 +2,41 @@
 // made product quantity should be reduced, if quanity is less than 5 reorder request should be raised.
 // Design an interface and classes for the same.
 
+
+
 interface ShopInterface {
-  id: number;
-  product: string;
-  purchase();
-}
-
-class FirstClass implements ShopInterface {
-  id: number;
-  product: string;
-  purchase() {
-    var ProductArr = [
-      { id: 1, product: "one" },
-      { id: 2, product: "two" },
-      { id: 3, product: "three" },
-      { id: 4, product: "four" },
-      { id: 5, product: "five" },
-      { id: 6, product: "six" },
-    ];
-
-    for (var a of ProductArr) {
-      console.log(a.product);
-    }
-    ProductArr.forEach(function (val) {
+    id: number;
+    product: string;
+    purchase();
+  }
+  
+  class FirstClass implements ShopInterface {
+    id: number;
+    product: string;
+    purchase() {
+  
+  
+      var ProductArr = [
+        { id: 1, product: "one" },
+        { id: 2, product: "two" },
+        { id: 3, product: "three" },
+        { id: 4, product: "four" },
+        { id: 5, product: "five" },
+        { id: 6, product: "six" },
+      ];
+  
+  
+      for (var a of ProductArr) {
+          console.log(a.product);
+        }
+        ProductArr.forEach(function (val) {
       console.log(val.product);
     });
-
+  
+    
     console.log("welcome to store");
     var question = "Y";
-
+  
     switch (question) {
       case "Y":
         if (ProductArr.length < 5) {
@@ -48,9 +54,19 @@ class FirstClass implements ShopInterface {
         console.log("wrong input press only Y or N ");
         break;
     }
+  
+  
+      }
+        
+  
+      
   }
-}
-
-let Start1: FirstClass = new FirstClass();
-
-Start1.purchase();
+  
+  
+  
+  let Start1:FirstClass = new FirstClass();
+  
+  Start1.purchase();
+  
+  
+  
